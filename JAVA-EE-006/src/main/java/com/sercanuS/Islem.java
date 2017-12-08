7927d8154dd24d3ef292cde232520f48fc389975
sercan göger <sercan.goger@gmail.com>
2017-12-08 14:34:12 +0200

    bu benim ilk gonderim yapıyorum kızmaca darılmaca yok
    Change-Id: I3bc22a69eecf301bb3c3cb5a43bdc2cc8a24e732
    Signed-off-by: sercan göger <sercan.goger@gmail.com>

---
+++ JAVA-EE-006/src/main/java/com/sercanuS/Islem.java
@@ -0,0 +1,16 @@
+package com.sercanuS;
+
+import java.util.Arrays;
+
+public class Islem {
+
+	static int veriler[] = {1,2,3};
+	
+	
+	public static void goster(){
+		
+		System.out.println(Arrays.toString(veriler));
+	}
+	
+	
+}
